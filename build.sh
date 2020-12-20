29e6f50da7841f4c4eb5914a5176bdc7dbf1fafe
Balakov <mike@mikestimpson.com>
2020-12-20 17:32:27 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+build_dir=$(pwd)
+echo "building site at $build_dir"
+
+/Users/kute/gdrive/Code/StaticSiteGenerator/MacBinary/StaticSiteGenerator $build_dir
---
+++ monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+build_dir=$(pwd)
+echo "building site at $build_dir"
+
+/Users/mikestimpson/Google\ Drive\ File\ Stream/My\ Drive/Mike/Code/StaticSiteGenerator/MacBinary/StaticSiteGenerator "$build_dir" --watch --serve
