54b68adb15743c6b4fa49a18d20dbac4ad1b64b0
Mike Stimpson <mikestimpson@Mikes-MacBook-Pro.local>
2020-12-20 20:45:01 +0000

    Initial commit

--- monitor.sh
+++ monitor.sh
@@ -2,4 +2,4 @@
 build_dir=$(pwd)
 echo "building site at $build_dir"
 
-/Users/mikestimpson/Google\ Drive\ File\ Stream/My\ Drive/Mike/Code/StaticSiteGenerator/MacBinary/StaticSiteGenerator "$build_dir" --watch --serve
+/Users/mikestimpson/Google\ Drive\ File\ Stream/My\ Drive/Mike/Code/StaticSiteGenerator/MacBinary/StaticSiteGenerator --input "$build_dir" --output "$build_dir/docs" --watch --serve
