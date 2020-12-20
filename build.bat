3c5e54f5bd8b10b2652abec5bde872d531436d0e
Balakov <mike@mikestimpson.com>
2020-12-20 17:55:04 +0000

    Build site to github repository path

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ set build_dir=%cd%
 echo building site at %build_dir%
 
 cd /D D:\GoogleDrive\Mike\Code\StaticSiteGenerator\bin\Debug\net5.0
-StaticSiteGenerator.exe "%build_dir%"
+StaticSiteGenerator.exe --input "%build_dir%" --output "%build_dir%/../mikestimpson.com/"
