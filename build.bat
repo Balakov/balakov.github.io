be8d446c5b43b0399aae28d6ea43ce55e469196a
Balakov <mike@mikestimpson.com>
2026-02-12 17:39:08 +0000

    New design

--- build.bat
+++
@@ -1,7 +0,0 @@
-echo off
-set build_dir=%cd%
-echo building site at %build_dir%
-
-cd /D C:\CloudFiles\Code\StaticSiteGenerator\bin\Debug\net6.0
-StaticSiteGenerator.exe --input "%build_dir%" --output "%build_dir%\docs"
-pause
